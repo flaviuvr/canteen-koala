244a4fc80d4c6bbf126eb94511f64a98ce59a45f
flaviuvr <flav.raita@gmail.com>
2021-05-10 10:49:57 +0300

    Add login functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,8 +14,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # reset_session
-      # log_in @user
+      reset_session
+      log_in @user
       redirect_to @user
     else
       render 'new'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   root 'main_page#landing'
   get 'main_page/home'
   get 'main_page/landing'
@@ -6,6 +7,10 @@ Rails.application.routes.draw do
   resources :users
 
   get '/signup', to: 'users#new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
   get '/landing', to: 'main_page#landing'
   get '/home', to: 'main_page#home'
 end
