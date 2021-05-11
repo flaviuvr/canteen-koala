5d9627b567356dc0a9e79ed8bf074d0b1a3487b8
flaviuvr <flav.raita@gmail.com>
2021-05-11 15:29:26 +0300

    Add advanced login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,7 @@ class SessionsController < ApplicationController
     if user&.authenticate(params[:session][:password])
       reset_session
       log_in user
+      remember user
       redirect_to user
     else
       flash.now[:danger] = 'Invalid user/password combination. Try again'
@@ -14,7 +15,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,28 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
+
   validates :name, presence: true, length: { maximum: 30 }
   validates :email, presence: true, length: { maximum: 255 }, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true
 
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
+
+  def self.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, remember_token)
+  end
+
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
 end
