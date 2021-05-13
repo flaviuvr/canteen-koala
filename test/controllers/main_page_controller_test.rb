36754d54d73e4b181cfaba3d0c3cd2c0982ed80d
flaviuvr <flav.raita@gmail.com>
2021-05-13 18:26:45 +0300

    Delete unused files

--- test/controllers/main_page_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class MainPageControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
-    get main_page_home_url
-    assert_response :success
-  end
-end
