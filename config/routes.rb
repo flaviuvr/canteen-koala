99bbd4aa87eac6ec66eb3777c44075260a45ade9
flaviuvr <flav.raita@gmail.com>
2021-05-14 13:06:02 +0300

    Fix routes for products

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'sessions/new'
   root 'main_page#landing'
-  resources :products, only: %i[index new show edit] do
+  resources :products, only: %i[index new show edit create update destroy] do
     post :add_to_cart, on: :member
     delete :remove_from_cart, on: :member
     put :remove_cart_items, on: :member
