f51df471d7b33906772de717277daa23c065f684
flaviuvr <flav.raita@gmail.com>
2021-05-21 11:46:03 +0300

    Mock users in mailer previews

--- spec/mailers/previews/user_mailer_preview.rb
+++ spec/mailers/previews/user_mailer_preview.rb
@@ -3,14 +3,20 @@ class UserMailerPreview < ActionMailer::Preview
 
   # Preview this email at http://localhost:3000/rails/mailers/user_mailer/account_activation
   def account_activation
-    user = User.last
+    user = User.new(name: 'User',
+                    email: 'user@preview.org',
+                    password: 'previewpass',
+                    password_confirmation: 'previewpass')
     user.activation_token = User.new_token
     UserMailer.account_activation(user)
   end
 
   # Preview this email at http://localhost:3000/rails/mailers/user_mailer/password_reset
   def password_reset
-    user = User.first
+    user = User.new(name: 'User',
+                    email: 'user@preview.org',
+                    password: 'previewpass',
+                    password_confirmation: 'previewpass')
     user.reset_token = User.new_token
     UserMailer.password_reset(user)
   end
